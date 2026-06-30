67a5dc25a37ef2842d2ad72c61afd08063cba531
peheimschockturnier-sudo <peheimschockturnier@gmail.com>
2026-06-30 20:01:21 +0200

    Update status.js

--- js/status.js
+++ js/status.js
@@ -1,29 +1,30 @@
 const WEBAPP_URL = "https://script.google.com/macros/library/d/1xTEpuui5he7elMlSdXtDCbpjDIdU5Ow7aEc2rmSJHT1cejj7FUxUczZW/5";
 
-const maxTeilnehmer= 30;
+const maxTeilnehmer = 30;
 
 fetch(WEBAPP_URL)
 .then(response => response.json())
 .then(data => {
 
+    console.log("Antwort:", data);
+
     const angemeldet = data.teilnehmer;
 
     const frei = maxTeilnehmer - angemeldet;
 
-    const prozent = (angemeldet / maxGruppen) * 25;
+    const prozent = (angemeldet / maxTeilnehmer) * 25;
 
     document.getElementById("progressBar").style.width = prozent + "%";
 
-    if (angemeldet >= maxTeilnehmer) {
-
-        document.getElementById("statusText").innerHTML =
-        "🔴 Das Turnier ist ausgebucht!";
-
-    } else {
-
-        document.getElementById("statusText").innerHTML =
+    document.getElementById("statusText").innerHTML =
         `${angemeldet} von ${maxTeilnehmer} Plätzen vergeben<br>Noch ${frei} Plätze frei`;
 
-    }
+})
+.catch(error => {
+
+    console.error(error);
+
+    document.getElementById("statusText").innerHTML =
+        "❌ Fehler beim Laden der Teilnehmerzahl.";
 
 });
